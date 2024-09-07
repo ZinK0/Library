fe2b8b7528847458c64336460c023f47b500d5f2
Zin KO <zinko.it@outlook.com>
2024-09-08 00:11:01 +0630

    Added function for files to apped in Project Folder

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -42,12 +42,17 @@ document.addEventListener("DOMContentLoaded", () => {
   function loadModule() {
     // object.keys will return keys array of dirStructure [Module_01,Module_02]
     Object.keys(dirStructures).forEach((module) => {
+      console.log("Module => ", module);
+
       //   moduleList.classList.add("module-lists");
       const moduleName = document.createElement("li");
       moduleName.innerText = module;
+
       moduleName.addEventListener("click", () => {
         toggleModule(module, moduleName);
       });
+
+      // append the Module Name to existing module List.
       moduleList.appendChild(moduleName);
     });
   }
@@ -55,21 +60,47 @@ document.addEventListener("DOMContentLoaded", () => {
   function toggleModule(module, moduleName) {
     if (moduleName.classList.contains("active")) {
     } else {
-      console.log(module);
       const projectFolders = dirStructures[module];
 
+      //add toggled state
       moduleName.classList.add("active");
       const projectList = document.createElement("ul");
       projectList.classList.add("project-lists");
 
       Object.keys(projectFolders).forEach((project) => {
-        console.log(project);
         const projectName = document.createElement("li");
         projectName.innerText = project;
+
+        projectName.addEventListener("click", () => {
+          toggleFiles(module, project, projectName);
+        });
+
+        // append the project name to ul(projectLists)
         projectList.appendChild(projectName);
       });
+      // append projectList next to toggledModule
       moduleName.insertAdjacentElement("afterend", projectList);
     }
+  }
+
+  function toggleFiles(module, project, projectName) {
+    const fileFolders = dirStructures[module][project];
+    //   console.log(fileFolders);
+    const fileList = document.createElement("ul");
+    fileList.classList.add("project-files");
+
+    fileFolders.forEach((file) => {
+      const fileName = document.createElement("li");
+      fileName.innerText = removeExtension(file);
+      fileList.appendChild(fileName);
+    });
+
+    // we need to append the ul>li list to toggled Project
+    projectName.insertAdjacentElement("afterend", fileList);
+  }
+
+  function removeExtension(fileName) {
+    return fileName.split(".").slice(0, -1).join("."); // Remove file extension
   }
 
   loadModule();
